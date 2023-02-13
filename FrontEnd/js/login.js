9bf0d1094ff0ff231d00a7ecf361b11c5995c827
Maurine Duet <maurineduet@gmail.com>
2023-02-13 16:20:06 +0100

    first commit

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,34 @@
+/* Formulaire de connexion */
+
+const userLogin = document.querySelector("#login form")
+userLogin.addEventListener("submit", async e => {
+  e.preventDefault()
+
+  const data = new FormData(userLogin)
+
+  const userInfos = {
+    email: data.get("email"),
+    password: data.get("password")
+  }
+
+  console.log(userInfos)
+
+  const chargeUtile = JSON.stringify(userInfos)
+
+  let response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", "accept": "application/json"},
+    body: chargeUtile
+  })
+
+  let result = await response.json()
+
+  if (result.userId) {
+    location = "index.html"
+    const token = result.token
+  } else {
+    alert("Erreur dans l’identifiant ou le mot de passe.")
+  }
+
+})
+
