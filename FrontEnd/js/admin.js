9a68b03d57879d65bd352db9388f47d8ff37a09d
Maurine Duet <maurineduet@gmail.com>
2023-02-14 18:06:01 +0100

    Ajout de la seconde page de la modale et add du js front end qui la fait fonctionner

--- FrontEnd/js/admin.js
+++ FrontEnd/js/admin.js
@@ -1,9 +1,11 @@
 
 const adminBar = document.querySelector('.header-modal')
 const log = document.querySelector('#log')
+const headerBase = document.querySelector('.header-base')
 
 if (sessionStorage.getItem("token")) {
     adminBar.classList.remove('hidden')
+    headerBase.style.padding = "100px 0 50px 0"
     log.textContent = "log out"
     log.addEventListener('click', e => {
         e.preventDefault()
--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -5,17 +5,61 @@ const modalBox = document.querySelector('#modal')
 const modalHeader = document.querySelector('.header-modal')
 
 modalButton.addEventListener('click', e => {
-    modalBox.style.display = null
+    modalBox.classList.remove('hidden')
+    modalFirstPage.classList.remove("hidden")
+    modalSecondPage.classList.add("hidden")
     modalBox.setAttribute('aria-hidden', false)
     modalBox.setAttribute('aria-modal', true) 
 } )
 
-/* Fermer la modale */
+/* Fermer la modale sur la croix */
 
-const closeIcon = document.querySelector('#cross')
+
+const closeIcons = document.querySelectorAll('.cross')
+
+closeIcons.forEach(closeIcon  => {
 
 closeIcon.addEventListener('click' , e => {
-    modalBox.style.display = "none"
+    modalBox.classList.add('hidden')
     modalBox.setAttribute('aria-hidden', true)
     modalBox.setAttribute('aria-modal', false)
+})
+
+})
+
+/* Fermer la modale en dehors 
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+
+document.addEventListener('click', e => {
+
+    if (!modalBox.classList.contains('hidden')) {
+    const target = e.currentTarget
+  
+    if (target !== modalWrapper) {
+      modalBox.classList.add('hidden')
+    }
+}
+  
+}) */
+
+
+/* Passer à la seconde page de la modale */
+
+const addPicButton = document.querySelector('.add-pic-button')
+const modalFirstPage = document.querySelector('.modal-wrapper-page-1')
+const modalSecondPage = document.querySelector('.modal-wrapper-page-2')
+
+addPicButton.addEventListener("click", e => {
+    modalFirstPage.classList.add("hidden")
+    modalSecondPage.classList.remove("hidden")
+})
+
+/* Revenir à la première page de la modale */
+
+const backToFirstPage = document.querySelector('.arrow')
+
+backToFirstPage.addEventListener("click", e => {
+    modalFirstPage.classList.remove("hidden")
+    modalSecondPage.classList.add("hidden")
 })
